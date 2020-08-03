07f4351fdefcc1515bf58a896fa6a504f2e78914
Nicholas Eberts <nicholas.eberts@yahoo.com>
2020-08-03 18:01:43 -0400

    Update main.go

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	dc, ok := datacenters[region]
 
 	fmt.Fprintf(w, `<!DOCTYPE html>
-	<h4>Welcome from Google Cloud datacenters at:<h4>`)
+	<h4>Hola from Google Cloud datacenters at:<h4>`)
 	if !ok {
 		// cannot determine datacenter from zone, just use zone name
 		fmt.Fprintf(w, `<h1>%s!</h1>`, computeZone)
